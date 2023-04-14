2216ac5b29e594d0bb022153588ca9bceceff277
manasghandat <ghandatmanas@gmail.com>
2023-04-14 20:06:13 +0530

    Add function to read disk and file

--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -11,7 +11,7 @@ typedef uint8_t bool;
 /*
 * Bootsector struct
 * It contains info about the boot image
-* Used f
+* Used for providing various argument values about disk
 */
 typedef struct 
 {
@@ -43,8 +43,27 @@ typedef struct
 } __attribute__((packed)) BootSector;
 
 
+typedef struct 
+{
+    uint8_t Name[11];
+    uint8_t Attributes;
+    uint8_t _Reserved;
+    uint8_t CreatedTimeTenths;
+    uint16_t CreatedTime;
+    uint16_t CreatedDate;
+    uint16_t AccessedDate;
+    uint16_t FirstClusterHigh;
+    uint16_t ModifiedTime;
+    uint16_t ModifiedDate;
+    uint16_t FirstClusterLow;
+    uint32_t Size;
+} __attribute__((packed)) DirectoryEntry;
+
 // BootSector global variable
 BootSector g_BootSector;
+uint8_t * g_Fat = NULL;
+// Contains root directory and is an array of directory entry
+DirectoryEntry* g_RootDirectory = NULL;
 
 /*
 * This function reads reads boot sector from disk and stores it in global variable
@@ -53,8 +72,84 @@ BootSector g_BootSector;
 * Return value: boolean
 *   returns if the read was sucessful or not
 */
-bool readBootSector(FILE *disk){
+bool readBootSector(FILE *disk)
+{
     return fread(&g_BootSector,sizeof(g_BootSector),1,disk)>0;
+}
+
+
+/*
+* This function reads the sectors from the disk into the buffer
+* Parameters:
+*   disk      : disk image from which data is read
+*   lda       : sector number 
+*   count     : number of sectors to read
+*   bufferOut : pointer on where to store the data
+* Return value: boolean
+*   returns if the read was sucessful or not
+*/
+bool readSectors(FILE *disk, uint32_t lba, uint32_t count, void* bufferOut)
+{
+    bool ok = true;
+    ok = ok && (fseek(disk,lba * g_BootSector.BytesPerSector, SEEK_SET)==0);
+    ok = ok && (fread(bufferOut,g_BootSector.BytesPerSector,count,disk)==count);
+}
+
+
+/*
+* This function reads file allocation table into memory.
+* This is stored in the global variable `g_Fat`
+* Parameters:
+*   disk      : disk image from which data is read
+* Return value: boolean
+*   returns if the read was sucessful or not
+*/
+bool readFat(FILE *disk)
+{
+    g_Fat = (uint8_t *)malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
+    return readSectors(disk,g_BootSector.ReservedSectors,g_BootSector.SectorsPerFat,g_Fat);
+}
+
+/*
+* This function allocates memory for root directory and reads its content
+* Parameters:
+*   disk : disk image from which data is read
+* Return value: boolean
+*   returns if the read was sucessful or not
+*/
+bool readRootDirectory(FILE *disk)
+{
+    //Calculation of the begning of sector 
+    uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount;
+    uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount;
+    uint32_t sectors = (size / g_BootSector.BytesPerSector);
+    if (size % g_BootSector.BytesPerSector >0)
+    {
+        sectors ++;
+    }
+    g_RootDirectory = (DirectoryEntry *) malloc(sectors * g_BootSector.BytesPerSector);
+    return readSectors(disk,lba,sectors,g_RootDirectory);
+}
+
+/*
+* This function finds the file in file in the root directory
+* Parameters:
+*   name : name of the directory
+* Return Value: DirectoryEntry*
+*   returns a pointer to corresponding directory entry
+*   if the pointer is not found return NULL
+*/
+DirectoryEntry* findFile(const char* name)
+{
+    for (uint32_t i = 0; i < g_BootSector.DirEntryCount; i++)
+    {
+        if (memcmp(name, g_RootDirectory[i].Name, 11) == 0)
+        {
+            return &g_RootDirectory[i];
+        }
+        
+    }
+    
 }
 
 int main(int argc, char **argv)
@@ -81,9 +176,37 @@ int main(int argc, char **argv)
     // Throw error message
     if(!readBootSector(disk))
     {
-        fprintf(stderr,"Cannot open boot sector!",argv[1]);
-        return -1;
+        fprintf(stderr,"Cannot open boot sector!");
+        return -2;
     }
 
+    // Throw error message
+    if(!readFat(disk))
+    {
+        fprintf(stderr,"Cannot read FAT file!");
+        free(g_Fat);
+        return -3;
+    }
+
+    // Throw error message
+    if(!readRootDirectory(disk))
+    {
+        fprintf(stderr,"Cannot read FAT file!");
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -4;
+    }
+
+    DirectoryEntry* fileEntry = findFile(argv[2]);
+    if(!fileEntry)
+    {
+        fprintf(stderr,"Could not find file %s!",argv[1]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -5;
+    }
+
+    free(g_Fat);
+    free(g_RootDirectory);
     return 0;
 }
