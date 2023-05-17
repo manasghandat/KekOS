633c0d8ac510dc845046cdb97cc3c584c824302c
manasghandat <ghandatmanas@gmail.com>
2023-05-17 23:21:13 +0530

    Add state machine for printf

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -24,3 +24,56 @@ void puts(const char* str)
         *str++;
     }
 }
+
+#define PRINTF_STATE_NORMAL 0
+#define PRINTF_STATE_LENGTH 1
+#define PRINTF_STATE_LENGTH_SHORT 2
+#define PRINTF_STATE_LENGTH_LONG 3
+#define PRINTF_STATE_SPEC 4
+
+#define PRINTF_LENGTH_DEFAULT 0
+#define PRINTF_LENGTH_SHORT_SHORT 1
+#define PRINTF_LENGTH_SHORT 2
+#define PRINTF_LENGTH_LONG 3
+#define PRINTF_LENGTH_LONG_LONG 4
+
+void printf(const char* fmt,...)
+{
+    int* argp = (int *)&fmt;
+    argp++;
+    int state = PRINTF_STATE_NORMAL;
+    int length;
+    while (*fmt)
+    {
+        switch (state)
+        {
+        case PRINTF_STATE_NORMAL:
+            switch (*fmt)
+            {
+            case '%':
+                state = PRINTF_STATE_LENGTH;
+                break;
+            default:
+                putc(*fmt);
+                break;
+            }
+            break;
+        
+        case PRINTF_STATE_LENGTH:
+            switch (*fmt)
+            {
+                case 'h':
+
+                break;
+
+                case 'l':
+
+                break;
+            }
+
+            break;
+        }
+        fmt++;
+    }
+    
+}
